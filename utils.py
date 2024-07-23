b1eaefdd652ccd1dafdac71a8b9f9133761edaa6
Mirza Athar Baig <athar.baig6167@gmail.com>
2024-07-23 11:54:25 -0500

    added function to extract compressed files

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import fnmatch
 import logging
+import zipfile
+import tarfile
 # This function searches for all vhdl files and check if they have a keyword in it. 
 # Intended to search all signle module vhdl source files
 def search_files(directory, keyword):
@@ -24,3 +26,22 @@ def search_files(directory, keyword):
     # Log files where the keyword was not found
     for file_path in files_without_keyword:
         logging.info(file_path)
+
+
+# extract all compressed files in a directory and subdirectories, extract them all at
+# the same location.
+def extract_compressed_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            try:
+                if filename.endswith('.zip'):
+                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                        zip_ref.extractall(root)
+                    print(f"Extracted {file_path}")
+                elif filename.endswith('.tar') or filename.endswith('.tar.gz') or filename.endswith('.tgz'):
+                    with tarfile.open(file_path, 'r') as tar_ref:
+                        tar_ref.extractall(root)
+                    print(f"Extracted {file_path}")
+            except Exception as e:
+                print(f"Failed to extract {file_path}: {e}")
